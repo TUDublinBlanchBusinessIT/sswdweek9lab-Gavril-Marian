d6b83beba12b2426471d93275cf8287b8a0c2f3b
B00165945 Gavril Marian <B00165945@mytudublin.ie>
2025-11-10 16:18:25 +0000

    Finished part 1, i managed to register and then log in with the same credentials

--- firebase.js
+++ firebase.js
@@ -2,12 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID",
-};
+const firebaseConfig = {  
+projectId: "mawk8-gavril21x",
+  appId: "1:477065779603:web:f94c8f5d4a39da9c3af066",
+  storageBucket: "mawk8-gavril21x.firebasestorage.app",
+  apiKey: "AIzaSyAfPK5Jqri5jr1wtRq-pkx4fXJmziik7aU",
+  authDomain: "mawk8-gavril21x.firebaseapp.com",
+  messagingSenderId: "477065779603",
+  measurementId: "G-HSR6G41G47",
+}
+
+;
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
